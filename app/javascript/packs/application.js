d092a317624520d268f81353271ac8af1c5341d0
Cezary Kłos <cezary.klos@gmail.com>
2022-04-20 20:50:30 +0200

    add animate.css and bulma-toast

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,7 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
+global.bulmaToast = require("bulma-toast")
 import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 
@@ -10,3 +10,4 @@ Rails.start()
 ActiveStorage.start()
 
 require("packs/layout/navbar_burger.js")
+
