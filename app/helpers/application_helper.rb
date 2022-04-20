aaf5ed00a395a4217bd97640966e97438c628e00
Cezary Kłos <cezary.klos@gmail.com>
2022-04-20 21:12:16 +0200

    add bulma_flash helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,19 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def bulma_flash
+    flash.each_with_object([]) do |(type, message), flash_messages|
+      type = 'is-success' if type == 'notice'
+      type = 'is-danger' if type == 'alert'
+      text = "<script>toastr.#{type}('#{message}', '', { closeButton: true, progressBar: true })</script>"
+
+      text = "<script>bulmaToast.toast({
+      message: '#{message}',
+      type: '#{type}',
+      dismissible: true,
+      animate: { in: 'fadeIn', out: 'fadeOut' }})</script>"
+
+      flash_messages << text.html_safe if message
+    end.join("\n").html_safe
+  end
 end
