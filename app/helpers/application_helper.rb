873e0f03398c0ad1527b6e3b462d49c71398f1cb
Cezary Kłos <cezary.klos@gmail.com>
2022-04-21 14:45:50 +0200

    deal with rubocop

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,14 +7,14 @@ module ApplicationHelper
       type = 'is-success' if type == 'notice'
       type = 'is-danger' if type == 'alert'
 
-      text = "<script>bulmaToast.toast({
+      text = "bulmaToast.toast({
       message: '#{message}',
       type: '#{type}',
       dismissible: true,
       duration: #{duration},
-      animate: { in: 'fadeIn', out: 'fadeOut' }})</script>"
+      animate: { in: 'fadeIn', out: 'fadeOut' }})"
 
-      flash_messages << text.html_safe if message
-    end.join("\n").html_safe
+      flash_messages << text if message
+    end.join("\n")
   end
 end
