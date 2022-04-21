63ba54da33df8ef0f609917fc72bcea9ccdc6ba5
Cezary Kłos <cezary.klos@gmail.com>
2022-04-21 22:27:59 +0200

    config routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'pages/dashboard'
-  end
-  devise_for :admin_users, path: 'admin'
+  devise_for :admin_users, path: 'admin', controllers: { sessions: 'admin_users/sessions' }
   devise_for :employees
   resources :kudos
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  
   root to: 'kudos#index'
-
-  get '/admin', to: redirect('/admin/sign_in')
+  namespace :admin do
+    root to: redirect('/admin/pages/dashboard')
+    get 'pages/dashboard'
+  end
 
 end
