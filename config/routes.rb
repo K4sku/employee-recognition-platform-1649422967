cd17493edd21a8e3e7533cd2dc4ca57dfc402ab4
Cezary Kłos <cezary.klos@gmail.com>
2022-04-21 19:21:18 +0200

    add AdminUser devise model

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :kudos
+  devise_for :admin_users, path: 'admin'
   devise_for :employees
+  resources :kudos
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: 'kudos#index'
+
+  get '/admin', to: redirect('/admin/sign_in')
+
 end
