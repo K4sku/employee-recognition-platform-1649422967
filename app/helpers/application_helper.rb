139e744cf7c695ca2960d320ec8387bbe2722eaf
Cezary Kłos <cezary.klos@gmail.com>
2022-04-21 13:31:40 +0200

    set variable duration for flash

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,7 @@
 module ApplicationHelper
   def bulma_flash
     flash.each_with_object([]) do |(type, message), flash_messages|
+      duration = type == 'notice' ? '3000' : '600000'
       type = 'is-success' if type == 'notice'
       type = 'is-danger' if type == 'alert'
 
@@ -10,6 +11,7 @@ module ApplicationHelper
       message: '#{message}',
       type: '#{type}',
       dismissible: true,
+      duration: #{duration},
       animate: { in: 'fadeIn', out: 'fadeOut' }})</script>"
 
       flash_messages << text.html_safe if message
