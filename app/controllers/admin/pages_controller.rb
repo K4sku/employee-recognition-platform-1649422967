4aba812e0dd67140db94b7378d1f7bb150516500
Cezary Kłos <cezary.klos@gmail.com>
2022-04-22 15:29:17 +0200

    show kudos on dashboard

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -1,6 +1,15 @@
 module Admin
   class PagesController < ApplicationController
     before_action :authenticate_admin_user!
-    def dashboard; end
+    def dashboard
+      # GET /kudos
+      @kudos = Kudo.all.order('id DESC')
+    end
+  end
+
+  private
+
+  def authorize!
+    raise AuthorizationError unless admin_user_signed_in?
   end
 end
