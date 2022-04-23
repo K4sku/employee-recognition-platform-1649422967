a1db25d2cdc97e895af513b4fc19b3ef07152e51
Cezary Kłos <cezary.klos@gmail.com>
2022-04-24 00:13:39 +0200

    set default number_of_available_kudos value

--- db/migrate/20220423175037_add_number_of_available_kudos_to_employees.rb
+++ db/migrate/20220423175037_add_number_of_available_kudos_to_employees.rb
@@ -1,5 +1,5 @@
 class AddNumberOfAvailableKudosToEmployees < ActiveRecord::Migration[6.1]
   def change
-    add_column :employees, :number_of_available_kudos, :integer, null: false
+    add_column :employees, :number_of_available_kudos, :integer, null: false, default: '10'
   end
 end
