8b0d975ee9bfe98169f403c98f3def2be580d652
Cezary Kłos <cezary.klos@gmail.com>
2022-04-23 19:56:04 +0200

    fix and add new field

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,20 +7,20 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Employee.create!([
-  {email: "adam@test.com", password: "adamadam", password_confirmation: "adamadam", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil},
-  {email: "tomasz@test.com", password: "tomasztomasz", password_confirmation: "tomasztomasz", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil},
-  {email: "test@test.com", password: "testtest", password_confirmation: "testtest", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil},
-  {email: "asd@test.com", password: "asdasd", password_confirmation: "asdasd", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil},
-  {email: "user@test.com", password: "useruser", password_confirmation: "useruser", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil}
+  {email: "adam@test.com", password: "adamadam", password_confirmation: "adamadam", number_of_available_kudos: '9'},
+  {email: "tomasz@test.com", password: "tomasztomasz", password_confirmation: "tomasztomasz", number_of_available_kudos: '9'},
+  {email: "test@test.com", password: "testtest", password_confirmation: "testtest", number_of_available_kudos: '9'},
+  {email: "asd@test.com", password: "asdasd", password_confirmation: "asdasd", number_of_available_kudos: '10'},
+  {email: "user@test.com", password: "useruser", password_confirmation: "useruser", number_of_available_kudos: '10'}
 ])
 
 Kudo.create!([
   {title: "Kudo 1", content: "Kudo 1 content", giver: Employee.find_by(email: 'adam@test.com'), reciever: Employee.find_by(email: 'tomasz@test.com')},
   {title: "Kudo 2", content: "Kudo 2 content", giver: Employee.find_by(email: 'test@test.com'), reciever: Employee.find_by(email: 'user@test.com')},
-  {title: "Kudo 3", content: "Kudo 3 content", giver: Employee.find_by(email: 'tomasz@test.com'), reciever: Employee.find_by(email: 'admin@test.com')}
+  {title: "Kudo 3", content: "Kudo 3 content", giver: Employee.find_by(email: 'tomasz@test.com'), reciever: Employee.find_by(email: 'adam@test.com')}
 ])
 
 Admin.create!([
-  email: "admin@test.com", password: "adminadmin", password_confirmation: "adminadmin", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil,
-  email: "admin2@test.com", password: "admin", password_confirmation: "admin", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil
+  {email: "admin@test.com", password: "adminadmin", password_confirmation: "adminadmin"},
+  {email: "admin2@test.com", password: "adminadmin", password_confirmation: "adminadmin"}
   ])
