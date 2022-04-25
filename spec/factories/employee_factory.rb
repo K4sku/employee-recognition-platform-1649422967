5fc686999c34bad6b6b53347d391883b98e7234d
Cezary Kłos <cezary.klos@gmail.com>
2022-04-25 14:58:03 +0200

    change email facotry

--- spec/factories/employee_factory.rb
+++ spec/factories/employee_factory.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :employee, aliases: %i[giver reciever] do
-    email
+    email { generate :employee_email }
     password { 'password' }
     number_of_available_kudos { 10 }
   end
--- spec/factories/factory_sequences.rb
+++ spec/factories/factory_sequences.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
-  sequence :email do |n|
+  sequence :employee_email do |n|
     "employee#{n}@test.com"
+  end
+  sequence :admin_email do |n|
+    "admin#{n}@test.com"
   end
 end
