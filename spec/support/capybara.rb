31a736a2ef2bb3884804142fa80e73e73155d05f
Cezary Kłos <cezary.klos@gmail.com>
2022-04-25 15:22:56 +0200

    ahh

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -6,11 +6,11 @@ Selenium::WebDriver.logger.ignore(:browser_options)
 
 Capybara.register_driver :headless_chrome do |app|
   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-      chromeOptions: { args: %w[--headless --disable-gpu] },
-      'goog:loggingPrefs': {
-          browser: 'ALL'
-      }
-    )
+    chromeOptions: { args: %w[--headless --disable-gpu] },
+    'goog:loggingPrefs': {
+      browser: 'ALL'
+    }
+  )
 
   options = ::Selenium::WebDriver::Chrome::Options.new
 
@@ -19,11 +19,11 @@ Capybara.register_driver :headless_chrome do |app|
   options.add_argument('--window-size=1400,1400')
 
   Capybara::Selenium::Driver.new(
-      app,
-      browser: :chrome,
-      desired_capabilities: capabilities,
-      options: options
-    )
+    app,
+    browser: :chrome,
+    desired_capabilities: capabilities,
+    options: options
+  )
 end
 
 Capybara.default_driver = :headless_chrome
