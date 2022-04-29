4d69c15d06083ff364782141fea748163aec326e
Cezary Kłos <cezary.klos@gmail.com>
2022-04-29 21:36:31 +0200

    fix: layout cropping on mobile devices

--- app/helpers/kudos_helper.rb
+++ app/helpers/kudos_helper.rb
@@ -1,5 +1,5 @@
 module KudosHelper
   def static_button_if_zero(avaible_kudos)
-    avaible_kudos.positive? ? 'button is-primary' : 'button is-static'
+    avaible_kudos.positive? ? 'button is-primary level-item' : 'button is-static level-item'
   end
 end
