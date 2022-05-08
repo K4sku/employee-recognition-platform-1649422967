be309a6e476e952c8e70bbfe524a43844c1776c9
Cezary Kłos <cezary.klos@gmail.com>
2022-05-08 21:55:24 +0200

    tests: add missing test case check that admin can delete employees that have kudos

--- spec/system/admins/employee/delete_spec.rb
+++ spec/system/admins/employee/delete_spec.rb
@@ -3,8 +3,6 @@ require 'rails_helper'
 describe 'admins/employee.delete', type: :system, js: true do
   before do
     driven_by(:rack_test)
-    create(:employee)
-    create(:employee)
     login_as(admin, scope: :admin)
   end
 
@@ -12,12 +10,38 @@ describe 'admins/employee.delete', type: :system, js: true do
   let(:admin) { create(:admin) }
 
   context 'when admin deletes' do
-    it 'show all employess' do
+    it 'removes employee' do
+      create(:employee)
+      create(:employee)
       visit admins_employees_path
       expect(page).to have_selector(:css, "div[id^='employee_']", count: 2)
       click_on 'Delete', match: :first
       expect(page).to have_current_path admins_employees_path
       expect(page).to have_selector(:css, "div[id^='employee_']", count: 1)
+    end
+
+    it 'removes kudos given by deleted employee' do
+      employee1 = create(:employee)
+      create(:kudo, giver: employee1)
+      visit admins_employees_path
+      expect(page).to have_selector(:css, "div[id^='employee_']", count: 2)
+      click_on 'Delete', match: :first
+      expect(page).to have_current_path admins_employees_path
+      expect(page).to have_selector(:css, "div[id^='employee_']", count: 1)
+      visit admins_pages_dashboard_path
+      expect(page).to have_selector(:css, "div[id^='kudo_']", count: 0)
+    end
+
+    it 'removes kudos recieved by deleted employee' do
+      employee1 = create(:employee)
+      create(:kudo, reciever: employee1)
+      visit admins_employees_path
+      expect(page).to have_selector(:css, "div[id^='employee_']", count: 2)
+      click_on 'Delete', match: :first
+      expect(page).to have_current_path admins_employees_path
+      expect(page).to have_selector(:css, "div[id^='employee_']", count: 1)
+      visit admins_pages_dashboard_path
+      expect(page).to have_selector(:css, "div[id^='kudo_']", count: 0)
     end
   end
 end
