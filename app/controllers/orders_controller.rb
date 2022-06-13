12ae61ffd17a17e3b6b04e71d905d94409bd900c
Cezary Kłos <cezary.klos@gmail.com>
2022-06-13 21:00:36 +0200

    refactor: :art: move validation logic from controller to model

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,18 +1,12 @@
 class OrdersController < EmployeeBaseController
   def create
-    reward = Reward.find(params[:reward])
-    if current_employee.points < reward.price
-      redirect_to reward_path(reward),
-                  alert: 'Not enough points for this reward' and return
-    end
-
     @order = Order.new(reward_id: order_params)
     @order.employee = current_employee
     if @order.save
       redirect_to rewards_path, notice: 'You have bought a reward!'
     else
-      flash[:notice] = 'Something went wrong'
-      redirect_back
+      flash[:alert] = @order.errors[:can_not_afford].join('. ')
+      redirect_back fallback_location: rewards_path
     end
   end
 
