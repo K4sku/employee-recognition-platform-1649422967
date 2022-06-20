2ced3e9d263de7f6dd589add384f4e7671251dc7
Cezary Kłos <cezary.klos@gmail.com>
2022-06-20 21:43:34 +0200

    feat: :sparkles: add index controller and set purchase_price on placing order
    add memonization

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,16 @@
 class OrdersController < EmployeeBaseController
+  def index
+    @orders = Order.where(employee: current_employee).includes(:reward)
+  end
+
   def create
     @order = Order.new(reward_id: order_params)
-    @order.employee = current_employee
-    if @order.save
+    order.employee = current_employee
+    order.set_purchase_price
+    if order.save
       redirect_to rewards_path, notice: 'You have bought a reward!'
     else
-      flash[:alert] = @order.errors[:can_not_afford].join('. ')
+      flash[:alert] = order.errors[:can_not_afford].join('. ')
       redirect_back fallback_location: rewards_path
     end
   end
@@ -13,6 +18,10 @@ class OrdersController < EmployeeBaseController
   private
 
   def order_params
-    params.require(:reward)
+    params.require(:reward_id)
+  end
+
+  def order
+    @order ||= Order.find(params[:order_id])
   end
 end
