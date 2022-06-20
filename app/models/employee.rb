356cc5d27ed4dfbecf77ba516fe69761787eca9a
Cezary Kłos <cezary.klos@gmail.com>
2022-06-20 19:47:32 +0200

    fix: :bug: prevents destroying rewards when employee is deleted

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -10,7 +10,7 @@ class Employee < ApplicationRecord
   has_many :given_kudos, class_name: 'Kudo', foreign_key: 'giver_id', inverse_of: 'giver', dependent: :destroy
   has_many :received_kudos, class_name: 'Kudo', foreign_key: 'reciever_id', inverse_of: 'reciever', dependent: :destroy
   has_many :orders, dependent: :destroy
-  has_many :rewards, through: :orders, dependent: :destroy
+  has_many :rewards, through: :orders
 
   def points
     received_kudos.count - rewards.sum(:price)
