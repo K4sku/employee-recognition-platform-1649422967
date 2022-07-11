58a5ca8875529c1610a201756c0c3dd196ad54a0
Cezary Kłos <cezary.klos@gmail.com>
2022-07-11 10:16:18 +0200

    feat: :sparkles: add deliver button to admins orders view
    button is disabled if item is delivered
    add action in controller

--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -1,7 +1,27 @@
 module Admins
   class OrdersController < BaseController
     def index
-      @orders = Order.all
+      @orders = Order.all.includes(:employee, :reward).order(status: :desc)
+      @undelivered_orders_count = count_undelivered
+    end
+
+    def deliver
+      authorize!
+      order.delivered_status!
+    end
+
+    private
+
+    def order
+      @order ||= Order.find(params[:id])
+    end
+
+    def authorize!
+      raise AuthorizationError unless admin_signed_in?
+    end
+
+    def count_undelivered
+      @orders.count { |order| order.status != 'delivered' }
     end
   end
 end
