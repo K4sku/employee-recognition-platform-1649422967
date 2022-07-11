0bdabaa86943f975a15a98aed121422e81bd1c55
Cezary Kłos <cezary.klos@gmail.com>
2022-07-11 22:18:43 +0200

    fix: :bug: use integers as enum in db
    change usage of psql custpm TYPE to using integers
    this way we are not breaking all tests without introducing new gem or modifying rails psql adapter

--- db/migrate/20220711064604_add_status_to_orders.rb
+++ db/migrate/20220711064604_add_status_to_orders.rb
@@ -1,17 +1,6 @@
 class AddStatusToOrders < ActiveRecord::Migration[6.1]
-  def up
-    execute <<-SQL
-      CREATE TYPE order_status AS ENUM ('placed', 'delivered', 'not_set');
-    SQL
-    add_column :orders, :status, :order_status
+  def change
+    add_column :orders, :status, :integer
     add_index :orders, :status
-  end
-
-  def down
-    remove_column :orders, :status
-    remove_index :orders, :status
-    execute <<-SQL
-      DROP TYPE order_status
-    SQL
   end
 end
