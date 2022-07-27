1eb9579b0740d8214a2e8999f704125c72400491
Cezary Kłos <cezary.klos@gmail.com>
2022-07-27 21:04:34 +0200

    fix: :bug: fix typo

--- spec/mailers/previews/order_preview.rb
+++ spec/mailers/previews/order_preview.rb
@@ -1,7 +1,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/order
 class OrderPreview < ActionMailer::Preview
   def order_delivered_email
-    order = Orders.first
+    order = Order.first
     OrderMailer.with(order: order).order_delivered_email
   end
 end
