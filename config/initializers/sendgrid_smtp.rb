4269ff3826b6f1ce8703c41ded2ecc64b8f8ad1e
Cezary Kłos <cezary.klos@gmail.com>
2022-07-28 15:26:47 +0200

    feat: :zap: do not load sendgrid smtp params on test env

--- config/initializers/sendgrid_smtp.rb
+++ config/initializers/sendgrid_smtp.rb
@@ -1,9 +1,11 @@
-ActionMailer::Base.smtp_settings = {
-  domain:         'cklos.com',
-  address:        "smtp.sendgrid.net",
-  port:            587,
-  authentication: :plain,
-  user_name:      'apikey',
-  enable_starttls_auto: true,
-  password:       Rails.application.credentials.sendgrid[:api_key]
-}
+if Rails.env.development? || Rails.env.production?
+  ActionMailer::Base.smtp_settings = {
+    domain:         'cklos.com',
+    address:        "smtp.sendgrid.net",
+    port:            587,
+    authentication: :plain,
+    user_name:      'apikey',
+    enable_starttls_auto: true,
+    password:       Rails.application.credentials.sendgrid[:api_key]
+  }
+end
