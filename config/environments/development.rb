926760dd052d99d2e8c9b99eb9d794f6fdd7f245
Cezary Kłos <cezary.klos@gmail.com>
2022-07-28 21:31:32 +0200

    fix: :hammer: do not send emails from development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,8 +44,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Set the delivery method from letter_opener gem https://github.com/ryanb/letter_opener
-  # config.action_mailer.delivery_method = :letter_opener
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :letter_opener
+  # config.action_mailer.delivery_method = :smtp
   
   config.action_mailer.perform_deliveries = true
 
