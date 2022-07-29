5e1712ccb023cf5515b23ca93cbba16ed3d77cdf
Cezary Kłos <cezary.klos@gmail.com>
2022-07-29 11:23:30 +0200

    fix: :bug: use factory to deliver mock Order object for email preview in development env
    mail preview crashed when there was no Orders in DB

--- spec/mailers/previews/order_preview.rb
+++ spec/mailers/previews/order_preview.rb
@@ -1,7 +1,10 @@
 # Preview all emails at http://localhost:3000/rails/mailers/order
+# loads FactoryBot to use build strategy
 class OrderPreview < ActionMailer::Preview
+  include FactoryBot::Syntax::Methods
+
   def order_delivered_email
-    order = Order.first
+    order = build(:order)
     OrderMailer.with(order: order).order_delivered_email
   end
 end
