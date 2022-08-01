6ebe5d9446f95aab2eeb448f1da691d1d7d60f34
Cezary Kłos <cezary.klos@gmail.com>
2022-08-01 13:46:28 +0200

    refactor: :art: replace include with calling classe's method directly

--- spec/mailers/previews/order_preview.rb
+++ spec/mailers/previews/order_preview.rb
@@ -1,10 +1,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/order
-# loads FactoryBot to use build strategy
 class OrderPreview < ActionMailer::Preview
-  include FactoryBot::Syntax::Methods
-
   def order_delivered_email
-    order = build(:order)
+    order = FactoryBot.build(:order)
     OrderMailer.with(order: order).order_delivered_email
   end
 end
