ddc463609486529e164c1698db5aea95e9c11001
Cezary Kłos <cezary.klos@gmail.com>
2022-08-02 16:19:04 +0200

    test: :white_check_mark: add system spec for adding available kudos

--- spec/system/admins/employee/add_kudo_spec.rb
+++ spec/system/admins/employee/add_kudo_spec.rb
@@ -13,4 +13,31 @@ describe 'Admin add avaible kudos to employees', type: :system do
       expect(page).to have_button('Add kudos')
     end
   end
+
+  context 'when input is corect' do
+    it 'increase avaible kudos and go to employee index' do
+      sign_in admin
+      visit add_kudos_admins_employees_path
+      fill_in 'Number of available kudos to add', with: '2'
+      expect do
+        click_on 'Add kudos'
+        employee.reload
+      end
+        .to change(employee, :number_of_available_kudos)
+        .by(2)
+        .and change { current_path }.to(admins_employees_path)
+    end
+  end
+
+  context 'when input is incorrect' do
+    it 'shows do not increase avaible kudos and stay on page' do
+      sign_in admin
+      visit add_kudos_admins_employees_path
+      fill_in 'Number of available kudos to add', with: '30'
+      expect { click_on 'Add kudos' }
+        .not_to change(employee, :number_of_available_kudos)
+      expect { click_on 'Add kudos' }
+        .not_to change { current_path }
+    end
+  end
 end
