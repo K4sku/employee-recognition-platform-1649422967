7237b91cfc2bcd603a245b2483ad9776ea9874b3
Cezary Kłos <cezary.klos@gmail.com>
2022-08-03 22:01:31 +0200

    test: add testing of button instead of path

--- spec/system/admins/employee/add_kudo_spec.rb
+++ spec/system/admins/employee/add_kudo_spec.rb
@@ -7,7 +7,8 @@ describe 'Admin add avaible kudos to employees', type: :system do
   context 'when visiting add_kudos_path' do
     it 'shows form' do
       sign_in admin
-      visit add_kudos_admins_employees_path
+      visit admins_root_path
+      click_on 'Add Kudos'
       expect(page).to have_css "form[action='/admins/employees/add_kudos'][method='post']"
       expect(page).to have_field('number_of_available_kudos_to_add', type: 'number')
       expect(page).to have_button('Add kudos')
@@ -17,7 +18,8 @@ describe 'Admin add avaible kudos to employees', type: :system do
   context 'when input is corect' do
     it 'increase avaible kudos and go to employee index' do
       sign_in admin
-      visit add_kudos_admins_employees_path
+      visit admins_root_path
+      click_on 'Add Kudos'
       fill_in 'Number of available kudos to add', with: '2'
       expect do
         click_on 'Add kudos'
@@ -32,7 +34,8 @@ describe 'Admin add avaible kudos to employees', type: :system do
   context 'when input is incorrect' do
     it 'shows do not increase avaible kudos and stay on page' do
       sign_in admin
-      visit add_kudos_admins_employees_path
+      visit admins_root_path
+      click_on 'Add Kudos'
       fill_in 'Number of available kudos to add', with: '30'
       expect { click_on 'Add kudos' }
         .not_to change(employee, :number_of_available_kudos)
