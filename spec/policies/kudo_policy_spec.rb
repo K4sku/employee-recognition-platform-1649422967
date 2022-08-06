47bb58489258e64a5b413da12a87cabf10ee4ff3
Cezary Kłos <cezary.klos@gmail.com>
2022-08-06 14:52:36 +0200

    test: :white_check_mark: add system and unit test for kudos edit,delete timer

---
+++ spec/policies/kudo_policy_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+require 'pundit/rspec'
+
+describe KudoPolicy do
+  include ActiveSupport::Testing::TimeHelpers
+
+  let!(:employee) { build(:employee) }
+  let(:kudo) { build(:kudo, giver: employee) }
+
+  permissions :giver? do
+    it 'grants access if user is kudo\' giver' do
+      expect(described_class).to permit(employee, kudo)
+    end
+  end
+
+  permissions :update?, :edit?, :destroy? do
+    it 'grants access if less then 5 minutes elapsed since kudo\'s creation' do
+      kudo = create(:kudo, giver: employee)
+      puts kudo.inspect
+      travel 3.minutes
+      expect(described_class).to permit(employee, kudo)
+    end
+
+    it 'denies access if less then 5 minutes elapsed since kudo\'s creation' do
+      kudo = create(:kudo, giver: employee)
+      travel 6.minutes
+      expect(described_class).not_to permit(employee, kudo)
+    end
+  end
+end
--- spec/system/kudo/edit_spec.rb
+++ spec/system/kudo/edit_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 describe 'Kudo.edit', type: :system, js: true do
+  include ActiveSupport::Testing::TimeHelpers
   before do
     create(:employee)
   end
@@ -37,6 +38,24 @@ describe 'Kudo.edit', type: :system, js: true do
         expect(page).to have_current_path kudo_path(owned_kudo)
         expect(page).to have_content 'Lorem ipsum dolor sit amet'
         expect(page).to have_content 'consectetur adipiscing elit. Sed dignissim dignissim urna vel ultrices.'
+      end
+
+      it 'fails after 5 minutes from kudo\'s creation' do
+        logged_employee = create(:employee)
+        sign_in logged_employee
+        owned_kudo = create(:kudo, giver: logged_employee)
+        visit edit_kudo_path(owned_kudo)
+        fill_in 'kudo_title', with: 'Lorem ipsum dolor sit amet'
+        fill_in 'kudo_content', with: 'consectetur adipiscing elit. Sed dignissim dignissim urna vel ultrices.'
+        last_option_reciever = page.find_field('Reciever').all('option').last.text
+        page.select last_option_reciever, from: 'kudo_reciever_id'
+        last_option_company_value = page.find_field('Company value').all('option').last.text
+        page.select last_option_company_value, from: 'kudo_company_value_id'
+        travel 6.minutes
+        click_on 'Save Kudo'
+        expect(page).to have_current_path root_path
+        expect(page).to have_content owned_kudo.title
+        expect(page).to have_content 'You can not perform this action.'
       end
     end
 
