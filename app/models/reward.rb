b138be2095b15dcd27d277b0f597b632b57e8318
Cezary Kłos <cezary.klos@gmail.com>
2022-08-09 23:47:42 +0200

    feat: :sparkles: add pagination to reward model and controller
    paginate 3 rewards per page

--- app/models/reward.rb
+++ app/models/reward.rb
@@ -1,4 +1,6 @@
 class Reward < ApplicationRecord
   validates :title, :description, :price, presence: true
   validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+
+  paginates_per 3
 end
