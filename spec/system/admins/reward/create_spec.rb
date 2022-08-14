a56690725aa1cd7ca566cb7f57a1f662064a962b
Cezary Kłos <cezary.klos@gmail.com>
2022-08-14 11:52:20 +0200

    test: update reward create spec

--- spec/system/admins/reward/create_spec.rb
+++ spec/system/admins/reward/create_spec.rb
@@ -1,12 +1,14 @@
 require 'rails_helper'
 
-describe 'Reward create action', type: :system do
+describe 'Reward create action', type: :system, js: true do
+  let(:admin) { create(:admin) }
+  let!(:reward) { build(:reward) }
+  let!(:category1) { create(:category) }
+  let!(:category2) { create(:category) }
+
   before do
     sign_in admin
   end
-
-  let(:admin) { create(:admin) }
-  let!(:reward) { build(:reward) }
 
   context 'when admin is on index page' do
     it 'New Reward link' do
@@ -16,28 +18,41 @@ describe 'Reward create action', type: :system do
     end
   end
 
-  context 'when creating valid reward' do
-    it 'saves reward' do
-      visit new_admins_reward_path
+  context 'when creating reward' do
+    before do
+      visit admins_root_path
+      page.find(:css, "div[test_id='admin_panel']").click
+      within(:css, "div[test_id='admin_panel']") do
+        click_on 'Rewards'
+      end
+      click_on 'New Reward'
+    end
+
+    it 'saves valid reward' do
       fill_in 'Title', with: reward.title
       fill_in 'Description', with: reward.description
       fill_in 'Price', with: reward.price
+      page.find(:css, "input[test_id='category_input']").click
+      page.find(:link, category1.title).click
+      page.find(:css, "input[test_id='category_input']").click
+      page.find(:link, category2.title).click
       click_on 'Save Reward'
       expect(page).to have_current_path admins_rewards_path
       expect(page).to have_content(reward.title)
       expect(page).to have_content(reward.description)
       expect(page).to have_content(reward.price)
+      expect(page).to have_content(category1.title)
+      expect(page).to have_content(category2.title)
     end
-  end
 
-  context 'when failing validations' do
-    it 'shows validation errors' do
-      visit new_admins_reward_path
-      click_on 'Save Reward'
-      expect(page).to have_css("form[action='/admins/rewards']")
-      expect(page).to have_content("Title can't be blank")
-      expect(page).to have_content("Description can't be blank")
-      expect(page).to have_content("Price can't be blank")
+    context 'when failing validations' do
+      it 'shows validation errors' do
+        page.find(:button, 'Save Reward').click
+        expect(page).to have_css("form[action='/admins/rewards']")
+        expect(page).to have_content("Title can't be blank")
+        expect(page).to have_content("Description can't be blank")
+        expect(page).to have_content("Price can't be blank")
+      end
     end
   end
 end
