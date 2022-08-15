6974632186f8b5fa04487e0cb782ff82aa076bd3
Cezary Kłos <cezary.klos@gmail.com>
2022-08-15 15:22:26 +0200

    fix: :bug: fix model validations

--- spec/models/category_rewards_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe CategoryReward, type: :model do
-  it { is_expected.to validate_uniqueness_of(:category_id).scoped_to(:reward_id) }
-end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,5 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Category, type: :model do
   it { is_expected.to validate_presence_of(:title) }
-  it { is_expected.to validate_uniqueness_of(:title).case_insensitive }
+
+  describe 'validations' do
+    subject { build(:category) }
+
+    it { is_expected.to validate_uniqueness_of(:title).case_insensitive }
+  end
 end
