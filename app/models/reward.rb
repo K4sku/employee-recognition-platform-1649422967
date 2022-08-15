6f3e2363d22baaf397081f20af8adef7094a4255
Cezary Kłos <cezary.klos@gmail.com>
2022-08-15 11:19:42 +0200

    feat: :sparkles: validate presence of categories

--- app/models/reward.rb
+++ app/models/reward.rb
@@ -4,6 +4,7 @@ class Reward < ApplicationRecord
 
   validates :title, :description, :price, presence: true
   validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+  validates :categories, presence: true
 
   paginates_per 3
 end
