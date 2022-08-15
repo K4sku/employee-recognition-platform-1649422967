eb56cb8ada20fb41d3cacc5aa74df2bb8a3b5660
Cezary Kłos <cezary.klos@gmail.com>
2022-08-15 15:22:54 +0200

    test:

--- spec/system/admins/caregories/destroy_spec.rb
+++ spec/system/admins/caregories/destroy_spec.rb
@@ -14,9 +14,13 @@ describe 'admins destroy Category', type: :system, js: true do
 
   context 'when admin is destroying category with no rewards' do
     it 'destroys category record' do
+      category = create(:category)
+      visit current_path
       expect(page).to have_selector(:css, "div[test_id='category_#{category.id}']")
       expect(page).to have_content 'Rewards with category: 0'
-      click_on 'Delete'
+      accept_alert do
+        click_on 'Delete'
+      end
       expect(page).to have_current_path admins_categories_path
       expect(page).not_to have_selector(:css, "div[test_id='category_#{category.id}']")
     end
@@ -24,11 +28,17 @@ describe 'admins destroy Category', type: :system, js: true do
 
   context 'when admin is destroying category with rewards' do
     it 'destroys category record' do
-      expect(page).to have_selector(:css, "div[test_id='category_#{category_with_rew.id}']")
-      expect(page).to have_content 'Rewards with category: 2'
-      click_on 'Delete'
+      category_with_rewards = create(:category_with_rewards)
+      visit current_path
+      expect(page).to have_selector(:css, "div[test_id='category_#{category_with_rewards.id}']")
+      within(page.find(:css, "div[test_id='category_#{category_with_rewards.id}']")) do
+        expect(page).to have_content 'Rewards with category: 2'
+        accept_alert do
+          click_on 'Delete'
+        end
+      end
       expect(page).to have_current_path admins_categories_path
-      expect(page).to have_selector(:css, "div[test_id='category_#{category_with_rew.id}']")
+      expect(page).to have_selector(:css, "div[test_id='category_#{category_with_rewards.id}']")
       expect(page).to have_content 'Categoty can not be deleted. There are rewards in this category.'
     end
   end
