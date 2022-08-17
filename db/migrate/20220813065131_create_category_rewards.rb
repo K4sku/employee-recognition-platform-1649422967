735f8047b7e007a1912134e1afe2f9b5764118f3
Cezary Kłos <cezary.klos@gmail.com>
2022-08-17 21:07:02 +0200

    fix: add timestamps to CatgoryRewards

--- db/migrate/20220813065131_create_category_rewards.rb
+++ db/migrate/20220813065131_create_category_rewards.rb
@@ -3,6 +3,8 @@ class CreateCategoryRewards < ActiveRecord::Migration[6.1]
     create_table :category_rewards do |t|
       t.references :category, index: true
       t.references :reward, index: true
+
+      t.timestamps
     end
   end
 end
