55cd70bc285f1105b6be31ae6d6f1c4ac0e8a9e1
Cezary Kłos <cezary.klos@gmail.com>
2022-08-19 22:31:05 +0200

    feat: :sparkles: add filtering displayed rewards by category

--- app/controllers/rewards_controller.rb
+++ app/controllers/rewards_controller.rb
@@ -1,9 +1,21 @@
 class RewardsController < EmployeeBaseController
   def index
-    @rewards = Reward.page(params[:page])
+    @rewards = if reward_params.key?('category')
+                 Reward.includes(:categories).where("categories.title = '#{reward_params[:category]}'").references(:categories)
+               else
+                 Reward.all
+               end
+    @rewards = @rewards.page(params[:page])
+    @categories = Category.pluck(:title)
   end
 
   def show
     @reward = Reward.find(params[:id])
+  end
+
+  private
+
+  def reward_params
+    params.permit(:category)
   end
 end
