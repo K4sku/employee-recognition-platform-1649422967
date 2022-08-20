54234fc4704b29179ff4fdd2e2697bf3cc79c22b
Cezary Kłos <cezary.klos@gmail.com>
2022-08-20 12:40:39 +0200

    refactor: :art: change is-avtice class conditional

--- app/presenters/filter_links_presenter.rb
+++ app/presenters/filter_links_presenter.rb
@@ -7,7 +7,7 @@ class FilterLinksPresenter
 
   def styled_link_to_filter_query(request, filter_argument)
     link_selected = link_selected?(request.params, filter_argument)
-    @view.content_tag :li, class: active_class(link_selected) do
+    @view.content_tag :li, class: { 'is-active': link_selected } do
       if link_selected
         @view.link_to filter_argument, controller: request.parameters[:controller], action: 'index'
       else
@@ -20,9 +20,5 @@ class FilterLinksPresenter
   def link_selected?(request_params, filter_argument)
     request_params.fetch(@filter_key, '') == filter_argument ||
       (filter_argument == @default_selected_link && !request_params.key?(@filter_key))
-  end
-
-  def active_class(link_selected)
-    'is-active' if link_selected
   end
 end
