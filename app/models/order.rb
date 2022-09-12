dcd0fed8dd78f41a2001d1b45201a8d1a018e5cd
Cezary Kłos <cezary.klos@gmail.com>
2022-09-12 20:16:53 +0200

    Add Order.to_csv method

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,4 @@
+require 'csv'
 class Order < ApplicationRecord
   belongs_to :employee
   belongs_to :reward
@@ -9,5 +10,16 @@ class Order < ApplicationRecord
     return unless reward.present? && employee.present? && (reward.price > employee.points)
 
     errors.add(:can_not_afford, 'You can not afford this reward')
+  end
+
+  def self.to_csv
+    attributes = %w[employee_id reward_id purchase_price status created_at updated_at]
+
+    CSV.generate(headers: true) do |csv|
+      csv << attributes
+      all.find_each do |order|
+        csv << attributes.map { |attribute| order.send(attribute) }
+      end
+    end
   end
 end
