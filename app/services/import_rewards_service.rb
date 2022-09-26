493c45c70101882013972f5812c6b5041fcf7913
Cezary Kłos <cezary.klos@gmail.com>
2022-09-26 21:03:20 +0200

    Fix multiline string concatenation in error

--- app/services/import_rewards_service.rb
+++ app/services/import_rewards_service.rb
@@ -79,8 +79,8 @@ class ImportRewardsService
   def transaction_with_error_handling(&block)
     ActiveRecord::Base.transaction(requires_new: true, &block)
   rescue ActiveRecord::ActiveRecordError => e
-    message = "#{'Error'.pluralize(e.record.errors.size)} for '#{e.record.title}':
-                  #{e.record.errors.full_messages.join('; ')}"
+    message = "#{'Error'.pluralize(e.record.errors.size)} for '#{e.record.title}': "\
+              "#{e.record.errors.full_messages.join('; ')}"
     failure(message)
   end
 
